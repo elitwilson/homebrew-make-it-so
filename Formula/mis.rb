4bb8af8607fce67749bcb3c45f57904802debe0d
Eli Wilson <elitwilson@gmail.com>
2025-06-01 13:57:43 -0400

    Add checksum for v0.1.3

--- Formula/mis.rb
+++ Formula/mis.rb
@@ -2,7 +2,7 @@ class Mis < Formula
   desc "A CLI framework that runs project-scoped, Deno-powered plugins for your dev workflows."
   homepage "https://github.com/elitwilson/make-it-so"
   url "https://github.com/elitwilson/make-it-so/releases/download/v0.1.4/mis-macos.tar.gz"
-  sha256 "<paste-sha256-here>"
+  sha256 "9db040126e6887ed9ec40ff997aee7ee8a1b1d4173eb77399c73c898d36dd28b"
   version "0.1.4"
 
   def install
