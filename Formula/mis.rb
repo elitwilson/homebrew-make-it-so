48646c8165b978d7d6ea758b3961198aaa1d13f2
Eli Wilson <elitwilson@gmail.com>
2026-01-30 10:34:45 -0500

    Update Mis formula to version 0.3.6

--- Formula/mis.rb
+++ Formula/mis.rb
@@ -1,9 +1,9 @@
 class Mis < Formula
   desc "A CLI framework that runs project-scoped, Deno-powered plugins for your dev workflows."
   homepage "https://github.com/elitwilson/make-it-so"
-  url "https://github.com/elitwilson/make-it-so/releases/download/v0.3.4/mis-macos.tar.gz"
-  sha256 "4757c7153f6c6219046d3912a7df67167f7c450fe3315d2f228224e16025a326"
-  version "0.3.4"
+  url "https://github.com/elitwilson/make-it-so/releases/download/v0.3.6/mis-macos.tar.gz"
+  sha256 "63ceceaf2e62a88ae2e696c74d7d1548725ffe9f2553668591962a33eef0500b"
+  version "0.3.6"
 
   def install
     bin.install "mis"
