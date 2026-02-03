600d9c6f867471de6cacf850ae781476aed2780c
Eli Wilson <elitwilson@gmail.com>
2026-02-03 12:45:12 -0500

    Change SHA256 checksum for mis package
    Updated the SHA256 checksum for the mis package.

--- Formula/mis.rb
+++ Formula/mis.rb
@@ -2,7 +2,7 @@ class Mis < Formula
   desc "A CLI framework that runs project-scoped, Deno-powered plugins for your dev workflows."
   homepage "https://github.com/elitwilson/make-it-so"
   url "https://github.com/elitwilson/make-it-so/releases/download/v0.3.8/mis-macos.tar.gz"
-  sha256 "5da3015e0b687664758b4f500dd362aa56424cf400a5f70f45db49234dfd3dc2"
+  sha256 "ca90e1743b37adcafda6f290482a584fc180f596838ea48da4e8b49b1e4fcf62"
   version "0.3.8"
 
   def install
